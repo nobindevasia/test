0a240a4564bc174512461cfb830eaf736d87636f
nobindevasia <61499859+nobindevasia@users.noreply.github.com>
2025-04-23 18:22:08 +0200

    Update FeatureSelectorFactory.cs

--- D2G.Iris.ML/FeatureEngineering/FeatureSelectorFactory.cs
+++ D2G.Iris.ML/FeatureEngineering/FeatureSelectorFactory.cs
@@ -21,8 +21,6 @@ namespace D2G.Iris.ML.FeatureEngineering
             {
                 FeatureSelectionMethod.None => new NoFeatureSelector(_mlContext),
                 FeatureSelectionMethod.Correlation => new CorrelationFeatureSelector(_mlContext),
-                FeatureSelectionMethod.PCA => new PCAFeatureSelector(_mlContext),
-                FeatureSelectionMethod.Forward => new ForwardFeatureSelector(_mlContext),
                 _ => throw new ArgumentException($"Unsupported feature selection method: {method}")
             };
         }
